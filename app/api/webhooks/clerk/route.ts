222dfe4e525460c4c4c7d43cc637d96598c06aec
saketsanadhya <saketsanadhya2003@gmail.com>
2024-06-05 16:58:22 +0530

    slight change

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
+    const user:any = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
@@ -87,7 +87,7 @@ export async function POST(req: Request) {
   if (eventType === "user.updated") {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
+    const user:any = {
       firstName: first_name,
       lastName: last_name,
       username: username!,
