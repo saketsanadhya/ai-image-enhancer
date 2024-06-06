75394e986226dec02b8cd668b05728e2cafc6a12
saketsanadhya <saketsanadhya2003@gmail.com>
2024-06-06 16:22:09 +0530

    fix??

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
- 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk','/api/webhooks/stripe']);
+
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
